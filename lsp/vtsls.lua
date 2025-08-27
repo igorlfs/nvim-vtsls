198d0162098ce57352e42a35365d647d9e26ada2
Igor <igorlfs@ufmg.br>
2025-08-27 18:27:26 -0300

    feat!: only override settings via lsp-config
    I get the other stuff from nvim-lspconfig, so why bother?

--- lsp/vtsls.lua
+++ lsp/vtsls.lua
@@ -1,31 +1,13 @@
-local cmd = { "vtsls", "--stdio" }
-
-if vim.fn.has("win32") == 1 then
-  cmd = { "cmd.exe", "/C", unpack(cmd) }
-end
-
 return {
-  cmd = cmd,
-  filetypes = {
-    "javascript",
-    "javascriptreact",
-    "javascript.jsx",
-    "typescript",
-    "typescriptreact",
-    "typescript.tsx",
-  },
-
-  root_markers = { "tsconfig.json", "jsonconfig.json", "package.json", ".git" },
-
-  settings = {
-    typescript = {
-      updateImportsOnFileMove = "always",
-    },
-    javascript = {
-      updateImportsOnFileMove = "always",
-    },
-    vtsls = {
-      enableMoveToFileCodeAction = true,
-    },
-  },
+	settings = {
+		typescript = {
+			updateImportsOnFileMove = "always",
+		},
+		javascript = {
+			updateImportsOnFileMove = "always",
+		},
+		vtsls = {
+			enableMoveToFileCodeAction = true,
+		},
+	},
 }
